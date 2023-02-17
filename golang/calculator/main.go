cd907c2fcafe0a197fa18947ec7f37bbf2f78d07
Victor Payno <vpayno@users.noreply.github.com>
2023-02-16 20:07:19 -0800

    golang/calculator: assignment template

---
+++ golang/calculator/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func calculate(a int, b int) []float64 {
+    // your code goes here
+    return results
+}
+
+func main() {
+    fmt.Println(calculate(20, 10))
+    fmt.Println(calculate(700, 70))
+}
