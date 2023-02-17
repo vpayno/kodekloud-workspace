fd5f51806fb3ca73db5e83e42bac950973afd55f
Victor Payno <vpayno@users.noreply.github.com>
2023-02-16 20:24:48 -0800

    golang/product: assignment complete

--- golang/product/main.go
+++ golang/product/main.go
@@ -3,7 +3,18 @@ package main
 import "fmt"
 
 func discountedPrice(product string, price float64) float64 {
-	// your code goes here
+	var result float64
+
+	switch product {
+	case "apples":
+		return (1 - 0.10) * price
+	case "bananas":
+		return (1 - 0.20) * price
+	default:
+		return price
+	}
+
+	return result
 }
 
 func main() {
