ec43dbfc272d5ea8cba1b9852343b758623aea30
Victor Payno <vpayno@users.noreply.github.com>
2023-02-16 20:09:35 -0800

    golang/calculator: assignment complete

--- golang/calculator/main.go
+++ golang/calculator/main.go
@@ -3,11 +3,31 @@ package main
 import "fmt"
 
 func calculate(a int, b int) []float64 {
-    // your code goes here
-    return results
+	results := []float64{}
+	results = append(results, sum(a, b))
+	results = append(results, difference(a, b))
+	results = append(results, product(a, b))
+	results = append(results, quotient(a, b))
+	return results
+}
+
+func sum(a, b int) float64 {
+	return float64(a + b)
+}
+
+func difference(a, b int) float64 {
+	return float64(a - b)
+}
+
+func product(a, b int) float64 {
+	return float64(a * b)
+}
+
+func quotient(a, b int) float64 {
+	return float64(a / b)
 }
 
 func main() {
-    fmt.Println(calculate(20, 10))
-    fmt.Println(calculate(700, 70))
+	fmt.Println(calculate(20, 10))
+	fmt.Println(calculate(700, 70))
 }
